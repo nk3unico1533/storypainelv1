be8b2b1aee28735b67f51cfe9d48fbe33fcd34e0
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 12:17:05 -0300

    Add Home component with StoryCarousel

---
+++ components/app/page.js
@@ -0,0 +1,9 @@
+import StoryCarousel from "@/components/stories/StoryCarousel";
+
+export default function Home() {
+  return (
+    <main className="w-full h-full overflow-hidden bg-black text-white">
+      <StoryCarousel />
+    </main>
+  );
+}

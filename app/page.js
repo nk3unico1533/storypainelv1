69ba1aa4103aae24b986a8555548e3217d6ac0f5
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 13:53:34 -0300

    Atualizar o page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
-import StoryCarousel from "@/components/stories/StoryCarousel";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
 import CpfConsulta from "@/components/stories/CpfConsulta";
 import CnpjConsulta from "@/components/stories/CnpjConsulta";
 import RgConsulta from "@/components/stories/RgConsulta";
@@ -9,20 +18,31 @@ import EmailConsulta from "@/components/stories/EmailConsulta";
 import CepConsulta from "@/components/stories/CepConsulta";
 
 export default function Home() {
-  const slides = [
-    <CpfConsulta key="cpf" />,
-    <CnpjConsulta key="cnpj" />,
-    <RgConsulta key="rg" />,
-    <TelefoneConsulta key="tel" />,
-    <PlacaConsulta key="placa" />,
-    <NomeConsulta key="nome" />,
-    <EmailConsulta key="email" />,
-    <CepConsulta key="cep" />,
-  ];
+  const router = useRouter();
+
+  // proteger rota
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) router.replace("/login");
+  }, [router]);
 
   return (
-    <main className="w-full h-screen bg-black text-white">
-      <StoryCarousel slides={slides} />
+    <main className="w-full h-screen bg-black text-white overflow-hidden">
+      <Swiper
+        direction="vertical"
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+        className="w-full h-full"
+      >
+        <SwiperSlide><CpfConsulta /></SwiperSlide>
+        <SwiperSlide><CnpjConsulta /></SwiperSlide>
+        <SwiperSlide><RgConsulta /></SwiperSlide>
+        <SwiperSlide><TelefoneConsulta /></SwiperSlide>
+        <SwiperSlide><PlacaConsulta /></SwiperSlide>
+        <SwiperSlide><NomeConsulta /></SwiperSlide>
+        <SwiperSlide><EmailConsulta /></SwiperSlide>
+        <SwiperSlide><CepConsulta /></SwiperSlide>
+      </Swiper>
     </main>
   );
 }

a58584160e2f8598f3b75ee9908ed49f845f8865
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 12:39:09 -0300

    Atualizar o page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import StoryCarousel from "@/components/stories/StoryCarousel";
+import CpfConsulta from "@/components/stories/CpfConsulta";
+import CnpjConsulta from "@/components/stories/CnpjConsulta";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen overflow-hidden">
-      <StoryCarousel />
-    </main>
+    <StoryCarousel>
+      {[
+        <CpfConsulta key="cpf" />,
+        <CnpjConsulta key="cnpj" />,
+      ]}
+    </StoryCarousel>
   );
 }

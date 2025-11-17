8632b2c0762ab8199f633b13bbe03dd648f7a72f
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 15:04:53 -0300

    Atualizar o page.js

--- app/page.js
+++ app/page.js
@@ -34,7 +34,6 @@ export default function Home() {
         modules={[Pagination]}
         className="w-full h-full"
       >
-        <SwiperSlide><CpfConsulta /></SwiperSlide>
 
       </Swiper>
     </main>

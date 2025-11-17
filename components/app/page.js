598e28345106268d748c3dc158d5654c5842c6a3
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 12:23:10 -0300

    Delete components directory

--- components/app/page.js
+++
@@ -1,9 +0,0 @@
-import StoryCarousel from "@/components/stories/StoryCarousel";
-
-export default function Home() {
-  return (
-    <main className="w-full h-full overflow-hidden bg-black text-white">
-      <StoryCarousel />
-    </main>
-  );
-}

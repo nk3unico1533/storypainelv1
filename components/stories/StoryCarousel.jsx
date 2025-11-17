7e1fbae8a950e3d7bccd438deb2d696cbb94bfb2
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 13:11:03 -0300

    Atualizar o StoryCarousel.jsx

--- components/stories/StoryCarousel.jsx
+++ components/stories/StoryCarousel.jsx
@@ -1,22 +1,31 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
 
-export default function StoryCarousel({ children }) {
+export default function StoryCarousel({ slides }) {
   return (
-    <div className="w-full h-screen bg-black flex items-center justify-center">
+    <div className="stories-wrapper w-full h-screen flex items-center justify-center">
       <Swiper
         modules={[Pagination]}
         pagination={{ clickable: true }}
-        spaceBetween={50}
-        slidesPerView={1}
+        spaceBetween={20}
+        slidesPerView={"auto"}
+        centeredSlides={true}
         className="w-full h-full"
       >
-        {children.map((child, i) => (
-          <SwiperSlide key={i}>{child}</SwiperSlide>
+        {slides.map((SlideComp, idx) => (
+          <SwiperSlide
+            key={idx}
+            className="flex items-center justify-center"
+            style={{ width: "80%", height: "100%" }} // slide width controls peek
+          >
+            <div className="slide-inner w-full h-full flex items-center justify-center">
+              {SlideComp}
+            </div>
+          </SwiperSlide>
         ))}
       </Swiper>
     </div>

d8367ca3720814ac1322b709736876ada21aaccc
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 18:28:53 -0300

    Atualizar o next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Permite exportar build standalone
   output: "standalone",
+
+  // Outras configurações que você já precise, se tiver
+  reactStrictMode: true,
+
+  // Se estiver usando imagens externas, pode configurar domains
+  images: {
+    domains: [],
+  },
 };
 
 module.exports = nextConfig;
